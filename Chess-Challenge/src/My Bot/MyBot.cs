cbc3552d32ad94184a17b74e50de607d4c9bf394
ZorzalG <nickwillimJT2@gmail.com>
2023-08-02 12:27:47 -0400

    test commit

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,6 +4,7 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        //test commit
         Move[] moves = board.GetLegalMoves();
         return moves[0];
     }
